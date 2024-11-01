712d0239a5605cd60991e1e79c7d13b62149fabc
Rakesh Arunachalam <rakesh@thoughtbot.com>
2024-11-01 16:34:19 +0000

    Organise tests into different kinds

--- vitest.database.config.ts
+++ vitest.database.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    dir: "test",
+    dir: "database",
     globals: true,
     environment: "node",
-    setupFiles: ["./test/setup.ts"],
+    setupFiles: ["./database/setup.ts"],
   },
 });
